413830397ffd30e6b9129605e5d1e91a0fe8d5a3
PatriciaPA99 <61124235+PatriciaPA99@users.noreply.github.com>
2020-03-06 18:06:01 -0600

    Comentarios y operadores
    Elimine las partes que no eran necesarias y agregue operadores

--- main.cpp
+++ main.cpp
@@ -8,28 +8,42 @@
 #include <math.h>
 using namespace std;
 
-char desifrar(char s[2][14], char clave){
-    char num;
-    if(clave == s[1][0])
-        num = s[0][0];
-    if(clave == s[1][1])
-        num = s[0][1];
-    if(clave == s[1][2])
-        num = s[0][2];
-    if(clave == s[1][3])
-        num = s[0][3];
-    if(clave == s[1][4])
-        num = s[0][4];
-    if(clave == s[1][5])
-        num = s[0][5];
-    if(clave == s[1][6])
-        num = s[0][6];
-    if(clave == s[1][7])
-        num = s[0][7];
-    if(clave == s[1][8])
-        num = s[0][8];
-    if(clave == s[1][9])
-        num = s[0][9];
+int descifrarNum(string clave, string numCif){
+  int largoNumCif = numCif.length();
+  int numDes = 0, multiplicando;
+  for(int i = 0; i < largoNumCif; i++){
+    multiplicando = clave.find(numCif[i]);
+    numDes += multiplicando * pow(10, largoNumCif - i - 1);
+  }
+  return numDes;
+}
+
+int resultadoOpe(string clave, string &oper, int num1, int num2){
+  int resultado;
+  int posicion = clave.find(oper);
+  switch(posicion){
+    case 10:
+      resultado = num1 + num2;
+      oper = "+";
+      return resultado;
+    break;
+    case 11:
+      resultado = num1 - num2;
+      oper = "-";
+      return resultado;
+    break;
+    case 12:
+      resultado = num1 * num2;
+      oper = "*";
+      return resultado;
+    break;
+    case 13:
+      resultado = num1 / num2;
+      oper = "/";
+      return resultado;
+    break;
+  }
+  return 0;
 }
 
 int main()
@@ -40,35 +54,24 @@ int main()
     cout << "Escribe el nombre del archivo de salida: ";
     cin >> salida;
     ifstream inputFile;
-    inputFile.open("Datos.txt");
-    string codigo, numC1, operC, numC2;
+    inputFile.open(entrada);
+    ofstream outputFile;
+    outputFile.open(salida);
     char carac;
-    char clave[2][14] = {{'0','1','2','3','4','5','6','7','8','9'}};
-    string contra;
+    string clave;
     for(int i = 0; i < 14; i++){
         inputFile >> carac;
-        clave[1][i] = carac;
-        contra += carac;
+        clave += carac;
     }
-    for(int i = 0; i < 2; i++){
-        for(int j = 0; j < 14; j++){
-            cout << clave[i][j] << "    " ;
-        }
-        cout << endl;
-    }
-    cout << "CONTRA  " << contra << endl;
-    int largoNum1, num1, decod, num;
+    int numDesci1, numDesci2, resulOpe;
+    string numC1, operC, numC2;
     while(inputFile >> numC1 >> operC >> numC2){
-        int largoNum1 = numC1.length();
-        num1 = 0;
-        for(int i = 0; i < largoNum1; i++){
-            num = contra.find(numC1[i]);
-            cout << "num " << num << endl;
-            num1 += num * pow(10, largoNum1 - i - 1);
-            cout << "num1" << " += (" << num << " * " << pow(10, largoNum1 - i - 1) << endl;
-        }
-        cout << numC1 << " = " << num1 << endl;
+      numDesci1 = descifrarNum(clave, numC1);
+      numDesci2 = descifrarNum(clave, numC2);
+      resulOpe = resultadoOpe( clave, operC, numDesci1, numDesci2);
+      outputFile << "(" <<numDesci1 << ") " << operC << " (" << numDesci2 << ") " << " = " <<resulOpe << endl;
     }
     inputFile.close();
+    outputFile.close();
     return 0;
 }
